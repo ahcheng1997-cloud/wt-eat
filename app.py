1c072840d0ed559308123b6cd0c4a688ab01c44a
ahcheng1997-cloud <abc19970124@gmail.com>
2026-04-06 22:56:48 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+def send_bafang_menu(token):
+flex = {
+"type": "flex",
+"altText": "八方雲集菜單",
+"contents": {
+"type": "bubble",
+"body": {
+"type": "box",
+"layout": "vertical",
+"contents": [
+{"type": "text", "text": "八方雲集", "weight": "bold", "size": "xl"},
+{"type": "button",
+"action": {"type": "message", "label": "🥟 鍋貼", "text": "鍋貼"}},
+{"type": "button",
+"action": {"type": "message", "label": "🥟 水餃", "text": "水餃"}},
+{"type": "button",
+"action": {"type": "message", "label": "🥣 酸辣湯", "text": "酸辣湯"}}
+]
+}
+}
+}
+
+```
+send(token, [flex])
+```
