b98da4b3a25ff14dc82764a6c751f3bd1e69645d
ahcheng1997-cloud <abc19970124@gmail.com>
2026-04-06 23:42:20 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import requests
 import os
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 CHANNEL_ACCESS_TOKEN = "XlA2IPfLH7iJYrdyzyQJ4nsO/z2QtvN9KDK5hF4lYSbDXFuBHjjba6RDGboj9b3L5blXJMrgYt8BRjpBxpRE4HEPIpyITY0IqMAuTcvfe1FCoFN309KkIAZosJJcjwUEmVLFDUxqoeLJYbAF5kxmeQdB04t89/1O/w1cDnyilFU="
 
